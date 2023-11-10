73cc8e5df56fe6bc07f902d48295a8dcfa84c2ca
Elijah Hampton <elijahhampton@MBP-de-Elijah-2.lan>
2023-11-10 18:14:26 -0500

    Updates origin

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,20 @@
 int main() {
     Database database;
     ZCashApi api(database);
-    crow::SimpleApp app;
+    crow::SimpleApp<crow::CORSHandler> app;
+
     try {
+        auto& cors = app.get_middleware<crow::CORSHandler>();
+
+         cors
+      .global()
+        .headers("X-Custom-Header", "Upgrade-Insecure-Requests")
+        .methods("POST"_method, "GET"_method)
+      .prefix("/cors")
+        .origin("ec2-18-222-152-234.us-east-2.compute.amazonaws.com")
+      .prefix("/nocors")
+        .ignore();
+        
         api.init(app, Config::getDatabaseName(), Config::getDatabaseUser(), Config::getDatabasePassword(), Config::getDatabaseHost(), Config::getDatabasePort());
         app.port(8000).multithreaded().run();
     } catch(const std::exception& e) {
